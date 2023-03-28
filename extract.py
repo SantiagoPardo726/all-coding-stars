db1b83a1a9438df326a5dc26a9f30bc306cb5826
Santiago Pardo <s.pardob@uniandes.edu.co>
2023-03-28 09:52:15 -0500

    ss

--- extract.py
+++ extract.py
@@ -1,8 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from langdetect import detect
+import cv2
+import urllib.request
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.wait import WebDriverWait
+import random
+import time
 
-def createDriver() -> webdriver.Chrome:
+def createDriver():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
@@ -17,11 +25,73 @@ def createDriver() -> webdriver.Chrome:
 
     return myDriver
 
-def getGoogleHomepage(driver: webdriver.Chrome) -> str:
-    driver.get("https://www.google.com")
-    return driver.page_source
+def verifyHindi(url:str):
+    try:
+        time.sleep(1)
+        webDriver = createDriver()
 
-def doBackgroundTask(inp):
-    print("Doing background task")
-    print(inp.msg)
-    print("Done")
+        webDriver.get(url)
+
+        text = webDriver.find_element(By.TAG_NAME,'body').text
+
+        language = detect(text)
+        
+        if language == 'hi':
+            return True
+        return False
+    except :
+        return True
+
+def verifyImage(url):
+    webDriver = createDriver()
+    webDriver.get(url)
+    ret = False
+    images = webDriver.find_elements(By.TAG_NAME,'img')
+    for image in images:
+        img_link = image.get_attribute('src')
+        try:
+            urllib.request.urlretrieve(img_link,"image.jpg")
+            img = cv2.imread("image.jpg")
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            laplacian_var = cv2.Laplacian(gray,cv2.CV_64F).var()
+            print(laplacian_var)
+            if laplacian_var > 5000:
+                ret = True
+        except :
+            pass
+    return ret
+
+def verifyDropDownMenu(url):
+    driver = createDriver()
+
+
+    driver.get(url)
+
+    try:
+        element = driver.find_element(By.XPATH,"//button[@data-name='LARGE_UP_MAIN_NAV_TRIGGER'][contains(text(), 'पाठ्यक्रम')]")
+
+   
+        actions = ActionChains(driver)
+        actions.move_to_element(element).perform()
+
+    
+        wait = WebDriverWait(driver, 10)
+
+        try:
+            expected = driver.find_element(By.XPATH, "//nav[contains(@class,'main-nav-dropdown') and contains(@class,'js-main-nav-dropdown') and contains(@class,'is-open')]")
+            return True
+        except:
+            return False
+    except :
+        return False
+    
+def verifyInside(url):
+    webDriver = createDriver()
+    webDriver.get(url)
+    links = webDriver.find_elements(By.TAG_NAME,"a")
+    for i in random.sample(links,5):
+        url = i.get_attribute("href")
+        print(verifyHindi(url))
+        if not verifyHindi(url):
+            return False
+    return True
--- main.py
+++ main.py
@@ -1,37 +1,58 @@
-from fastapi import FastAPI, BackgroundTasks, HTTPException
+from fastapi import FastAPI, Form,Request
+from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
+from fastapi.templating import Jinja2Templates
 from extract import *
-import os
+import extract
 
-
-SECRET = os.getenv("SECRET")
 
 #
 app = FastAPI()
 
-class Msg(BaseModel):
-    msg: str
-    secret: str
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
+@app.get("/",response_class=HTMLResponse)
+def form(request:Request):
+    return templates.TemplateResponse("index.html",{"request":request})
 
-async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
+@app.post("/",response_class=HTMLResponse)
+def form(request:Request, links: str = Form(...)):
+    print(links)
+    lst = links.split("\n")
+    message = ""
+    cont = 1
+    for i in lst:
+        print(i)
+        mes = getMessaje(i)
+        message += str(cont) + ")"+mes
+        cont += 1
+        
+        
+    return templates.TemplateResponse("index.html",{"request":request,"message":message})
+
+def getMessaje(url):
+    message = ""
+    if extract.verifyHindi(url):
+        pass
+    else:
+        return "Wrong page"
+    if extract.verifyImage(url):
+        pass
+    else:
+        message += "Images not high resolution; \n"
+    if extract.verifyInside(url):
+        pass
+    else:
+        message += "Inner pages not translated; \n"
+    if extract.verifyDropDownMenu(url):
+        pass
+    else:
+        message += "Javascript dropdown not working properly;\n"
+    if message == "":
+        return "Pass"
+    else:
+        return "Fail: "+message
 
 
-@app.get("/homepage")
-async def demo_get():
-    driver=createDriver()
-
-    homepage = getGoogleHomepage(driver)
-    driver.close()
-    return homepage
-
-@app.post("/backgroundDemo")
-async def demo_post(inp: Msg, background_tasks: BackgroundTasks):
-    
-    background_tasks.add_task(doBackgroundTask, inp)
-    return {"message": "Success, background task started"}
-    
 
 
